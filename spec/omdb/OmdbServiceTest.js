c326baba71a7eaec11ef50ca16a38676994e6d5e
jwang1 <sigkdd@yahoo.com>
2016-10-09 17:27:47 -0400

    adding the $http service, also need to inject _$httpBackend_ in tests.

--- spec/omdb/OmdbServiceTest.js
+++ spec/omdb/OmdbServiceTest.js
@@ -4,29 +4,44 @@ describe('omdb service', function() {
    var movieDataById = {"Title":"American Pie","Year":"1999","Rated":"R","Released":"09 Jul 1999","Runtime":"95 min","Genre":"Comedy","Director":"Paul Weitz, Chris Weitz","Writer":"Adam Herz","Actors":"Jason Biggs, Chris Klein, Thomas Ian Nicholas, Alyson Hannigan","Plot":"Four teenage boys enter a pact to lose their virginity by prom night.","Language":"English","Country":"USA","Awards":"9 wins & 14 nominations.","Poster":"https://images-na.ssl-images-amazon.com/images/M/MV5BMTg3ODY5ODI1NF5BMl5BanBnXkFtZTgwMTkxNTYxMTE@._V1_SX300.jpg","Metascore":"58","imdbRating":"7.0","imdbVotes":"317,667","imdbID":"tt0163651","Type":"movie","Response":"True"};
    
   var omdbApi = {};
+  var $httpBackend;
+  
+  beforeEach(module('omdb'));     // missing this one can cause $httpBackend not injected correctly
 
-   beforeEach(function() {
-      angular.mock.module('omdb'); 
-      angular.mock.inject(function(_omdbApi_) {
-          omdbApi = _omdbApi_;
-      });
-   });
+   beforeEach(inject(function(_omdbApi_, _$httpBackend_) {
+      omdbApi = _omdbApi_;
+     $httpBackend = _$httpBackend_;
+   }));
    
 //    it('# should return search movie data', function(angular) {  // can cause : Error: Timeout - Async callback was not invoked within timeout specified by jasmine.DEFAULT_TIMEOUT_INTERVAL.
    it('# should return search movie data', function() {
       
       // debugging
-      console.log(omdbApi.search('pie'));  
+    //   console.log(omdbApi.search('pie'));  
       
       // using ngMock dump to debug
-      console.log(angular.mock.dump(omdbApi.search('pie')));
+    //   console.log(angular.mock.dump(omdbApi.search('pie')));
       
       // also using Karma dump debug
-      dump(angular.mock.dump(omdbApi.search('pie')));
+    //   dump(angular.mock.dump(omdbApi.search('pie')));
 
     //   expect(omdbApi.search('pie')).toBe(movieData);     // after getting data from the Real service, "toBe" not be held any more 
     
-      expect(omdbApi.search('pie')).toEqual(movieData);
+      var rslt ;
+      
+      $httpBackend.when('GET', omdbApi.getSearchUrl())
+        .respond(200, movieData);
+      
+      omdbApi.search('american pie')
+        .then(function(data) {
+          rslt = data;
+        }
+      );
+      
+      $httpBackend.flush();
+      
+      expect(rslt).toEqual(movieData);
+      
    }); 
    
    it('# should return movie data by id', function() {
--- src/omdb/OmdbService.js
+++ src/omdb/OmdbService.js
@@ -1,9 +1,33 @@
 angular.module('omdb', [])
-    .factory('omdbApi', function() {
+    .factory('omdbApi', function($http, $q) {
         var service = {};
         
+        var baseUrl = 'http://www.omdbapi.com/?y=&plot=full&r=json&';
+        var searchUrl;
+        var findUrl;
+        
+        // for testing
+        service.getSearchUrl = function() {
+            return searchUrl;   
+        };
+        
+        service.getFindUrl = function() {
+            return findUrl;  
+        };
+        
         service.search = function(query) {
-            return {"Search":[{"Title":"American Pie","Year":"1999","Rated":"R","Released":"09 Jul 1999","Runtime":"95 min","Genre":"Comedy","Director":"Paul Weitz, Chris Weitz","Writer":"Adam Herz","Actors":"Jason Biggs, Chris Klein, Thomas Ian Nicholas, Alyson Hannigan","Plot":"Four teenage boys enter a pact to lose their virginity by prom night.","Language":"English","Country":"USA","Awards":"9 wins & 14 nominations.","Poster":"https://images-na.ssl-images-amazon.com/images/M/MV5BMTg3ODY5ODI1NF5BMl5BanBnXkFtZTgwMTkxNTYxMTE@._V1_SX300.jpg","Metascore":"58","imdbRating":"7.0","imdbVotes":"317,667","imdbID":"tt0163651","Type":"movie","Response":"True"}]};
+            var deferred = $q.defer();
+            searchUrl = baseUrl + 't=' + encodeURIComponent(query);
+            
+            console.log('searchUrl : ' + searchUrl);
+            
+            // $http.get()          // bug, not passing the url
+            $http.get(searchUrl)
+                .success(function(data) {
+                   angular.mock.dump('data from rest :  ' + data);
+                   deferred.resolve(data);  
+                });
+            return deferred.promise;
         };
         
         service.find = function(id) {
