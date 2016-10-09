c8c53113b2533dfc52974e35ebe6fb0fa2c8f158
jwang1 <sigkdd@yahoo.com>
2016-10-08 23:24:11 -0400

    Consolidate common parts of all tests;
    put common parts in beforeEach callback.

--- spec/omdb/OmdbServiceTest.js
+++ spec/omdb/OmdbServiceTest.js
@@ -3,24 +3,20 @@ describe('omdb service', function() {
    
    var movieDataById = {"Title":"American Pie","Year":"1999","Rated":"R","Released":"09 Jul 1999","Runtime":"95 min","Genre":"Comedy","Director":"Paul Weitz, Chris Weitz","Writer":"Adam Herz","Actors":"Jason Biggs, Chris Klein, Thomas Ian Nicholas, Alyson Hannigan","Plot":"Four teenage boys enter a pact to lose their virginity by prom night.","Language":"English","Country":"USA","Awards":"9 wins & 14 nominations.","Poster":"https://images-na.ssl-images-amazon.com/images/M/MV5BMTg3ODY5ODI1NF5BMl5BanBnXkFtZTgwMTkxNTYxMTE@._V1_SX300.jpg","Metascore":"58","imdbRating":"7.0","imdbVotes":"317,667","imdbID":"tt0163651","Type":"movie","Response":"True"};
    
+  var omdbApi = {};
+
+   beforeEach(function() {
+      angular.mock.module('omdb'); 
+      angular.mock.inject(function(_omdbApi_) {
+          omdbApi = _omdbApi_;
+      });
+   });
+   
 //    it('# should return search movie data', function(angular) {  // can cause : Error: Timeout - Async callback was not invoked within timeout specified by jasmine.DEFAULT_TIMEOUT_INTERVAL.
    it('# should return search movie data', function() {
-      var omdbApi = {};
-      
-      // 3 ways of mocking
-      // a. by anonymous obj 
-      // b. by module-name-string (as what's used here')
-      // c. by anonymous function 
-      angular.mock.module('omdb');
-      
-
-      angular.mock.inject(function(_omdbApi_) {
-         omdbApi = _omdbApi_; 
-      });
       
       // debugging
       console.log(omdbApi.search('pie'));  
-      
 
     //   expect(omdbApi.search('pie')).toBe(movieData);     // after getting data from the Real service, "toBe" not be held any more 
     
@@ -28,14 +24,6 @@ describe('omdb service', function() {
    }); 
    
    it('# should return movie data by id', function() {
-       var omdbApi = {};
-       
-       module('omdb');
-       
-       inject(function(_omdbApi_) {
-           omdbApi = _omdbApi_;
-       });
-       
        expect(omdbApi.find('tt016365')).toEqual(movieDataById);
    })
 });
